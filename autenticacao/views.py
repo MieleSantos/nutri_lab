cff3b0f0479123458e81b54b6cc72b282c1bf851
mielesantos <mielesnts@gmail.com>
2022-07-07 20:47:23 -0300

    add ativacao usuario
    Adicionando ativadao do usuario

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,11 +1,15 @@
+import os
 import re
+from hashlib import sha256
 
+from django.conf import settings
 from django.contrib import auth, messages
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.shortcuts import HttpResponse, redirect, render
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render
 
-from .utils import password_is_valid
+from .models import Ativacao
+from .utils import email_html, password_is_valid
 
 # Create your views here.
 
@@ -28,8 +32,26 @@ def cadastro(request):
                 username=username, email=email, password=senha, is_active=False
             )
             user.save()
+
+            token = sha256(f"{username}{email}".encode()).hexdigest()
+            ativacao = Ativacao(token=token, user=user)
+            ativacao.save()
+
             messages.add_message(
                 request, constants.SUCCESS, "Usuário cadastrado com sucesso"
+            )
+            path_template = os.path.join(
+                settings.BASE_DIR,
+                "autenticacao/templates/email/cadastro_confirmado.html",
+            )
+            email_html(
+                path_template,
+                "Cadastro confirmado",
+                [
+                    email,
+                ],
+                username=username,
+                link_ativacao=f"127.0.0.1:8000/auth/ativar_conta/{token}",
             )
             return redirect("/auth/logar")
         except Exception as e:
@@ -61,4 +83,18 @@ def logar(request):
 
 def sair(request):
     auth.logout(request)
+    return redirect("/auth/logar")
+
+
+def ativar_conta(request, token):
+    token = get_object_or_404(Ativacao, token=token)
+    if token.ativo:
+        messages.add_message(request, constants.WARNING, "Essa token já foi usado")
+        return redirect("/auth/logar")
+    user = User.objects.get(username=token.user.username)
+    user.is_active = True
+    user.save()
+    token.ativo = True
+    token.save()
+    messages.add_message(request, constants.SUCCESS, "Conta ativa com sucesso")
     return redirect("/auth/logar")
