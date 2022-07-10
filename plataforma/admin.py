b7ee2e27e455109fe90e4766b23c927a7d666df3
mielesantos <mielesnts@gmail.com>
2022-07-10 15:41:22 -0300

    add planto alimenta

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import DadosPaciente, Pacientes
+from .models import DadosPaciente, Opcao, Pacientes, Refeicao
 
 admin.site.register(Pacientes)
 admin.site.register(DadosPaciente)
+admin.site.register(Refeicao)
+admin.site.register(Opcao)
