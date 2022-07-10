ef131cba61fd23301ae90d827ddb77ba76534f1b
mielesantos <mielesnts@gmail.com>
2022-07-09 21:15:00 -0300

    add validador de campos

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,5 +1,9 @@
+from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import HttpResponse, render
+from django.contrib.messages import constants
+from django.shortcuts import HttpResponse, redirect, render
+
+from .models import Pacientes
 
 
 @login_required(login_url="/auth/logar")
@@ -12,5 +16,25 @@ def pacientes(request):
         idade = request.POST.get("idade")
         email = request.POST.get("email")
         telefone = request.POST.get("telefone")
+        if (
+            (len(nome.strip()) == 0)
+            or (len(sexo.strip()) == 0)
+            or (len(idade.strip()) == 0)
+            or (len(email.strip()) == 0)
+            or (len(telefone.strip()) == 0)
+        ):
+            messages.add_message(request, constants.ERROR, "Preencha todos os campos")
+            return redirect("/pacientes/")
 
+        if not idade.isnumeric():
+            messages.add_message(request, constants.ERROR, "Digite uma idade válida")
+            return redirect("/pacientes/")
+
+        pacientes = Pacientes.objects.filter(email=email)
+
+        if pacientes.exists():
+            messages.add_message(
+                request, constants.ERROR, "Já existe um paciente com esse E-mail"
+            )
+            return redirect("/pacientes/")
         return HttpResponse(f"{nome}, {sexo}, {idade}, {email}, {telefone}")
