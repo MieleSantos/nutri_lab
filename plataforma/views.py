a2c21f84486835416849c08e46a2dbcd905ce723
mielesantos <mielesnts@gmail.com>
2022-07-09 21:00:07 -0300

    add models e imagens

--- plataforma/views.py
+++ plataforma/views.py
@@ -4,4 +4,13 @@ from django.shortcuts import HttpResponse, render
 
 @login_required(login_url="/auth/logar")
 def pacientes(request):
-    return render(request, "pacientes.html")
+    if request.method == "GET":
+        return render(request, "pacientes.html")
+    elif request.method == "POST":
+        nome = request.POST.get("nome")
+        sexo = request.POST.get("sexo")
+        idade = request.POST.get("idade")
+        email = request.POST.get("email")
+        telefone = request.POST.get("telefone")
+
+        return HttpResponse(f"{nome}, {sexo}, {idade}, {email}, {telefone}")
